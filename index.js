d7eca053cabff4e9f2bc5ee6f8db09ab9b2aa62e
irvinbryson3-create <irvinbryson3@gmail.com>
2026-05-27 12:32:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40, fontFamily: "Arial", textAlign: "center" }}>
-      <h1>MB Precision Detailing</h1>
 
-      <p>Premium Car Detailing in Kentucky</p>
-
-      <h2>Contact</h2>
-      <p>Bryson: 502-612-6815</p>
-      <p>Maddox: 270-866-3229</p>
-
-      <h2>Instagram</h2>
-      <a href="https://www.instagram.com/mbpercisiondetailing_/" target="_blank">
-        Visit Our Instagram
-      </a>
-    </div>
-  );
-}
